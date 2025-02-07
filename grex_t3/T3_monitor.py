362bb905978e1566f6c56e9037334afc712dad39
sniedbalski <77023264+sniedbalski@users.noreply.github.com>
2025-02-07 15:34:24 -0500

    Update T3_monitor.py

--- grex_t3/T3_monitor.py
+++ grex_t3/T3_monitor.py
@@ -2,7 +2,7 @@
 ### Using Inotify to monitor /hdd/data/voltages/
 ### when a new .nc file is created, it calls the T3 plotting task, saves a png/pdf file, and posts it to the Slack candidates channel.
 ### Usage: poetry run python T3_monitor.py
-#test alyssa
+
 import inotify.adapters as ia
 import os
 import sys
@@ -11,8 +11,8 @@ import time
 import logging
 import cand_plotter
 
-logfile = '/home/user/zghuai/GReX-T3/services/T3_plotter.log'
-env_dir = "/home/user/zghuai/GReX-T3/grex_t3/" 
+logfile = '/home/cugrex/grex/t3/services/T3_plotter.log'
+env_dir = "/home/cugrex/grex/t3/grex_t3/" 
 mon_dir = "/hdd/data/voltages/" # monitoring dir
 dir_plot = "/hdd/data/candidates/T3/candplots/" # place to save output plots
 dir_fil  = "/hdd/data/candidates/T3/candfils/"  # place to save output filterbank files
@@ -51,8 +51,8 @@ def upload_to_slack(pdffile):
     
     try:
         # Upload the plot file to Slack
-        response = client.files_upload(
-            channels="candidates",
+        response = client.files_upload_v2(
+            channel="C07M2900B7W",
             file=pdffile,
             initial_comment=message
         )
@@ -60,9 +60,12 @@ def upload_to_slack(pdffile):
         print("Plot uploaded to Slack:", response["file"]["permalink"])
     except slk.errors.SlackApiError as err:
         print(f"Error uploading plot to Slack: {err}")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise 
 
 
-# Function to send a slack message (test)
+'''# Function to send a slack message (test)
 def send_to_slack(message):
 
     # set up slack client
@@ -86,17 +89,19 @@ def send_to_slack(message):
     # Define message parameters
     try:
         response = client.chat_postMessage(
-            channel="candidates",
+            channel="candidates-cuithaca",
             text=message
         )  
         
         print("Done", response.status_code)
     except slk.errors.SlackApiError as err:
         print(f"Error uploading plot to Slack: {err}")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise 
+'''
 
-
-
-def main(path, post=True):
+def main(path, post=True, test=False):
 
     # initiate an inotify instance
     i = ia.Inotify()
@@ -119,13 +124,13 @@ def main(path, post=True):
             if type_names == ['IN_CREATE']:
                 # if the filename ends with .nc
                 if filename.endswith('.nc'): # created a new .nc file
-                    logging.info(f"New NetCDF file created, waiting to plot.")
-                    print('Created {}'.format(filename))
+                    logging.info(f"New NetCDF file {filename}, waiting to plot.")
+                    #print('Created {}'.format(filename))
 
                     ### T3 goes here. 
                     c = filename.split('.')[0].split('/')[-1].split('-')[-1] # candidate ID
                     filename_json = c+".json"
-                    print('filename = ', filename_json)
+                    #print('filename = ', filename_json)
 
                     os.chdir(env_dir)
                     time.sleep(10)
@@ -135,29 +140,30 @@ def main(path, post=True):
                         fn_tempfil = dir_plot + "intermediate.fil" # output temporary .fil
                         fn_outfil = dir_fil + "cand{}.fil".format(c) # output dedispersed candidate .fil
                         (cand, tab) = cand_plotter.gen_cand(v, fn_tempfil, fn_outfil, c+'.json')
+                        if test==True:
+                            logging.info("Running test, no plots made")
+                        else:
+                            cand_plotter.plot_grex(cand, tab, c+".json") 
+                            logging.info("Done with cand_plotter.py")
 
-                        cand_plotter.plot_grex(cand, tab, c+".json") 
-                        logging.info("Done with cand_plotter.py")
+                            cmd = "rm {}".format(fn_tempfil)
+                            print(cmd)
+                            os.system(cmd)
+                            logging.info("Successfully plotted the canidate!")
 
-                        cmd = "rm {}".format(fn_tempfil)
-                        print(cmd)
-                        os.system(cmd)
-                        logging.info("Successfully plotted the canidate!")
+                            pdffile = dir_plot + "grex_cand"+filename_json.split('.')[0]+".png"
+
+                            if post==True:
+                                try:
+                                    upload_to_slack(pdffile) # upload to Slack #candidates channel
+                                    logging.info(f"Successfully posted to Slack #candidates!")
+                                except Exception as e:
+                                    logging.error("Error uploading candidate plot to Slack: %s", str(e))
+                                logging.info("DONE")
+
+                            del cand
                     except Exception as e:
                         logging.error("Error plotting candidates: %s", str(e))
-
-                    pdffile = dir_plot + "grex_cand"+filename_json.split('.')[0]+".png"
-                    print("saved in ", pdffile)
-
-                    if post==True:
-                        try:
-                            upload_to_slack(pdffile) # upload to Slack #candidates channel
-                            logging.info(f"Successfully posted to Slack #candidates!")
-                        except Exception as e:
-                            logging.error("Error uploading candidate plot to Slack: %s", str(e))
-                        logging.info("DONE")
-
-                    del cand
 
     except PermissionError:
         logging.error("Permission denied: Unable to create inotify test file.")
@@ -168,8 +174,9 @@ def main(path, post=True):
 
 if __name__ == '__main__':
     try:
-        post = True
-        main(mon_dir, post=post)
+        post = False
+        test = True
+        main(mon_dir, post=post, test=test)
     except Exception as e:
         print('Interrupted')
         logging.error("Interrupted: %s", str(e))
