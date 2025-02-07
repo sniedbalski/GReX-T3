949db63e99ae357b4c50788ba484da9e99faa011
sniedbalski <77023264+sniedbalski@users.noreply.github.com>
2025-02-07 13:59:47 -0500

    Delete grex_t3/mlmodel directory

--- grex_t3/mlmodel/RFIMLsettings.py
+++
@@ -1,6 +0,0 @@
-batch_size = 5
-
-data_path = '/home/user/connor/inferfi/datarfi_full.npy'
-out_path = './out/frbs_full/'
-model_path = '/home/user/connor/GReX-T3/grex_t3/mlmodel/RFImodel_UNet_May2024.pth'
-
